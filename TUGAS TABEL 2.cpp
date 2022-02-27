ff4c59286bc5409975b14c79f0d96ac492e113e0
marsahulwa <99006063+marsahulwa@users.noreply.github.com>
2022-02-27 21:10:52 +0700

    Update TUGAS TABEL 2.cpp

--- TUGAS TABEL 2.cpp
+++ TUGAS TABEL 2.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void tabel(int a, int b, int c, int *p1, int *p2, int *p3) {
+tabel(int a, int b, int c, int *p1, int *p2, int *p3) {
     cout << "|a: " << a << "| b: " << b << "| c: " << c  << "| p1: " << p1 << "| p2: " << p2 << "| p3: " << p3 << endl;
 }
 
 int main(){
-    //deklarasi variabel 
-    //nilai awal variabel
+    
     int a = 0, b = 0, c = 0; 
     int *p1 = 0, *p2 = 0, *p3 = 0;
 
